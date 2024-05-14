c0062357e5d754fd42c8548a020baadd2041149a
024SurasitKlahan <mamkungcup@gmail.com>
2024-05-14 11:47:01 +0700

    Add files via upload

---
+++ Toggle widget/ConvertApp.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class Scr1(Screen):
+    pass
+
+class Scr2(Screen): 
+    def convert(self,*args):
+        try:
+            num = int(self.ids.txt_number.text)  # แก้ชื่อ ID ให้ตรงกับที่กำหนดในไฟล์ .kv
+            if args[0].text=="base2":
+                self.ids.lbl_output.text = bin(num)[2:]
+            elif args[0].text=="base8":
+                self.ids.lbl_output.text = oct(num)[2:]
+            elif args[0].text=="base16":    
+                self.ids.lbl_output.text = hex(num)[2:]  # แก้ชื่อ ID ให้ตรงกับที่กำหนดในไฟล์ .kv
+        except ValueError:
+            self.ids.lbl_output.text = "Invalid input!"
+
+class UI(ScreenManager):
+    pass
+
+class ConvertApp(App):
+    def build(self):
+        return UI()
+
+if __name__ =="__main__":
+    ConvertApp().run()
